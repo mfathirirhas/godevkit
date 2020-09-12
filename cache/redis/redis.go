caf6ddb7a5d467921437fe6eb270f5da3ab9264a
mfathirirhas <mfathirirhas@gmail.com>
2020-09-13 05:23:59 +0700

    Add Redis functionalities

--- cache/redis/redis.go
+++ cache/redis/redis.go
@@ -1,7 +1,13 @@
 package redis
 
+/*
+	Wrappers for Redis functionalities.
+*/
+
 import (
 	"errors"
+	"fmt"
+	"time"
 
 	_redis "github.com/go-redis/redis/v7"
 )
@@ -9,11 +15,18 @@ import (
 var (
 	// ErrEmptyConnection empty client returned
 	ErrEmptyConnection = errors.New("redis: return empty connection")
+	intTrue            = int64(1)
+	intFalse           = int64(0)
 )
 
 type Redis struct {
-	client _redis.Cmdable
+	client iRedis
 	prefix string
+}
+
+type iRedis interface {
+	_redis.Cmdable
+	Watch(fn func(*_redis.Tx) error, keys ...string) error
 }
 
 type Opts struct {
@@ -44,6 +57,131 @@ func New(o *Opts) (*Redis, error) {
 	}, nil
 }
 
+func (r *Redis) pre(key string) string {
+	return fmt.Sprintf("%s:%s", r.prefix, key)
+}
+
 func (r *Redis) Ping() error {
 	return r.client.Ping().Err()
+}
+
+//--- Keys
+
+func (r *Redis) Exists(key string) bool {
+	return r.client.Exists(r.pre(key)).Val() == intTrue
+}
+
+func (r *Redis) Keys(pattern string) []string {
+	return r.client.Keys(pattern).Val()
+}
+
+func (r *Redis) Expire(key string, seconds int64) error {
+	return r.client.Expire(r.pre(key), time.Duration(seconds)).Err()
+}
+
+func (r *Redis) Persist(key string) error {
+	return r.client.Persist(r.pre(key)).Err()
+}
+
+func (r *Redis) Del(key string) error {
+	return r.client.Del(r.pre(key)).Err()
+}
+
+func (r *Redis) Dump(key string) string {
+	return r.client.Dump(r.pre(key)).Val()
+}
+
+//--- Strings
+
+func (r *Redis) Get(key string) string {
+	return r.client.Get(r.pre(key)).Val()
+}
+
+func (r *Redis) Set(key string, value interface{}) error {
+	return r.client.Set(r.pre(key), value, 0).Err()
+}
+
+func (r *Redis) Setex(key string, value interface{}, seconds int64) error {
+	return r.client.Set(r.pre(key), value, time.Duration(seconds)).Err()
+}
+
+func (r *Redis) SetNX(key string, value interface{}) error {
+	return r.client.SetNX(r.pre(key), value, 0).Err()
+}
+
+func (r *Redis) SetNXex(key string, value interface{}, seconds int64) error {
+	return r.client.SetNX(r.pre(key), value, time.Duration(seconds)).Err()
+}
+
+func (r *Redis) Increment(key string) error {
+	return r.client.Incr(r.pre(key)).Err()
+}
+
+func (r *Redis) Decrement(key string) error {
+	return r.client.Decr(r.pre(key)).Err()
+}
+
+//--- Hashes
+
+func (r *Redis) HGet(key, field string) string {
+	return r.client.HGet(r.pre(key), field).Val()
+}
+
+func (r *Redis) HGetAll(key string) map[string]string {
+	return r.client.HGetAll(r.pre(key)).Val()
+}
+
+func (r *Redis) HSet(key string, fieldvalue ...interface{}) error {
+	return r.client.HSet(r.pre(key), fieldvalue...).Err()
+}
+
+func (r *Redis) HSetNX(key, field string, value interface{}) error {
+	return r.client.HSetNX(r.pre(key), field, value).Err()
+}
+
+func (r *Redis) HVals(key string) []string {
+	return r.client.HVals(r.pre(key)).Val()
+}
+
+func (r *Redis) HDel(key, field string) error {
+	return r.client.HDel(r.pre(key), field).Err()
+}
+
+func (r *Redis) HExists(key, field string) bool {
+	return r.client.HExists(r.pre(key), field).Val()
+}
+
+//--- Sets
+
+func (r *Redis) SAdd(key string, values ...interface{}) error {
+	return r.client.SAdd(r.pre(key), values...).Err()
+}
+
+func (r *Redis) SMembers(key string) []string {
+	return r.client.SMembers(r.pre(key)).Val()
+}
+
+func (r *Redis) SRem(key string, members ...interface{}) error {
+	return r.client.SRem(r.pre(key), members...).Err()
+}
+
+func (r *Redis) ZAdd(key string, values ...interface{}) error {
+	score := float64(0)
+	z := make([]*_redis.Z, len(values))
+	for _, v := range values {
+		z = append(z, &_redis.Z{
+			Score:  score,
+			Member: v,
+		})
+		score++
+	}
+	return r.client.ZAdd(r.pre(key), z...).Err()
+}
+
+func (r *Redis) ZMembers(key string) []string {
+	return r.client.ZRange(r.pre(key), 0, -1).Val()
+}
+
+func (r *Redis) ZRem(key string, members ...interface{}) error {
+	return r.client.ZRem(r.pre(key), members...).Err()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mfathirirhas/godevkit
 
 go 1.15
 
-require github.com/go-redis/redis/v7 v7.4.0
+require (
+	github.com/go-redis/redis/v7 v7.4.0
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.6.1
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)
