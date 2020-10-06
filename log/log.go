8c52c73822876a25268c5d782fa0e0ee4d595a8b
mfathirirhas <mfathirirhas@gmail.com>
2020-10-06 17:08:34 +0700

    Add Set stdout and stderr and New logger
    - Set stdout and stderr used to set output file for stdout and stderr without concering about the package. It's to set global path for all kind of loggers.
    
    - New is for creating logger object with its own file path. In case of multiple log location.

--- log/log.go
+++ log/log.go
@@ -71,22 +71,85 @@ func Init(opts *Options) {
 	}
 }
 
-// set log to specified file with specified file descriptor.
-func set(logPath string, fd int) {
+func openFile(path string) (*os.File, error) {
 	// check if log directory exist, if not create one.
-	if _, err := os.Stat(filepath.Dir(logPath)); os.IsNotExist(err) {
-		if err = os.MkdirAll(filepath.Dir(logPath), 0744); err != nil {
-			log.Panicf("log: error setting log path: %v", err)
+	if _, err := os.Stat(filepath.Dir(path)); os.IsNotExist(err) {
+		if err = os.MkdirAll(filepath.Dir(path), 0744); err != nil {
+			return nil, err
 		}
 	}
-	logFile, err := os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0644)
+	logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0644)
 	if err != nil {
-		log.Panicf("log: failed opening log path: %v", err)
+		return nil, err
+	}
+
+	return logFile, nil
+}
+
+// set log to specified file with specified file descriptor.
+func set(logPath string, fd int) {
+	logFile, err := openFile(logPath)
+	if err != nil {
+		log.Panicf("log: failed to open file: %v", err)
 	}
 	defer logFile.Close()
 	if err = Dup2File(logFile, fd); err != nil {
 		log.Panicf("log: failed dup 2 to path %s with error: %v", logPath, err)
 	}
+}
+
+// Set to set stdout and/or stderr to a file.
+// If you just want to set the stdout and stderr location without using further of these package, e.g. using your own logger.
+func Set(stdOutFilePath string, stdErrFilePath string) {
+	if stdOutFilePath != "" {
+		set(stdOutFilePath, int(os.Stdout.Fd()))
+	}
+	if stdErrFilePath != "" {
+		set(stdErrFilePath, int(os.Stderr.Fd()))
+	}
+}
+
+// LoggerOpts options for creating new logger object.
+type LoggerOpts struct {
+	LogFilePath         string
+	EnableTimestamp     bool
+	TimestampFormat     string
+	EnableRuntimeCaller bool
+	IsDebug             bool
+}
+
+// New create logger object with specific location output using logrus as logger.
+// If you want to specify additional logger with its own location.
+func New(opts *LoggerOpts) (*_logrus.Logger, error) {
+	logger := _logrus.New()
+
+	logger.SetLevel(_logrus.InfoLevel)
+	if opts.IsDebug {
+		logger.SetLevel(_logrus.DebugLevel)
+	}
+	if opts.EnableRuntimeCaller {
+		logger.ReportCaller = true
+	}
+
+	logger.Formatter = new(_logrus.TextFormatter)
+	logger.Formatter.(*_logrus.TextFormatter).DisableTimestamp = true
+	if opts.EnableTimestamp {
+		logger.Formatter.(*_logrus.TextFormatter).DisableTimestamp = false
+	}
+	logger.Formatter.(*_logrus.TextFormatter).TimestampFormat = time.RFC3339
+	if opts.TimestampFormat != "" {
+		logger.Formatter.(*_logrus.TextFormatter).TimestampFormat = opts.TimestampFormat
+	}
+
+	logFile, err := openFile(opts.LogFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer logFile.Close()
+
+	logger.SetOutput(logFile)
+
+	return logger, nil
 }
 
 func formatStdout(fileName, funcName string, line int) string {
