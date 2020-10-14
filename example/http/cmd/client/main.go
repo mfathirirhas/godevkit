1b74d0705b8a1a4a11680661cac47e66d205a8fd
mfathirirhas <mfathirirhas@gmail.com>
2020-10-15 05:48:06 +0700

    Add http client instant functions example

--- example/http/cmd/client/main.go
+++ example/http/cmd/client/main.go
@@ -19,8 +19,12 @@ func main() {
 	})
 
 	Get(c)
-	PostURLEncoded(c)
+	PostForm(c)
 	PostJSON(c)
+
+	GetInstant()
+	PostFormInstant()
+	PostJSONInstant()
 }
 
 func Get(c *_client.Client) {
@@ -53,7 +57,7 @@ type RespJSON struct {
 	Param2 string `json:"param2"`
 }
 
-func PostURLEncoded(c *_client.Client) {
+func PostForm(c *_client.Client) {
 	postUrl := "http://localhost:8282/post2"
 	header := make(http.Header)
 	m := make(map[string]string)
@@ -97,6 +101,86 @@ func PostJSON(c *_client.Client) {
 	}
 	ctx := context.Background()
 	resp := c.PostJSON(ctx, postJson)
+	if resp.Err() != nil {
+		log.Println("Error: ", resp.Err())
+		return
+	}
+	str, err := resp.String()
+	if err != nil {
+		log.Println("Errorr: ", err)
+	}
+	fmt.Println(str)
+}
+
+func GetInstant() {
+	getUrl := "http://localhost:8282/get"
+
+	header := make(http.Header)
+	header.Set("request-id", "1")
+	urlvalues := make(url.Values)
+	urlvalues.Set("param1", "1")
+	urlvalues.Set("param2", "2")
+	urlvalues.Set("param3", "3")
+	getRequest := &_client.Request{
+		BaseURL:   getUrl,
+		Header:    header,
+		URLValues: urlvalues,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	resp := _client.Get(ctx, getRequest)
+	fmt.Println("Error: ", resp.Err())
+	str, err := resp.String()
+	if err != nil {
+		fmt.Println("wew: ", err)
+	}
+	fmt.Println("String: ", str)
+}
+
+func PostFormInstant() {
+	postUrl := "http://localhost:8282/post2"
+	header := make(http.Header)
+	m := make(map[string]string)
+	m["param1"] = "123"
+	m["param2"] = "456"
+	ctx := context.Background()
+	req := &_client.Request{
+		BaseURL: postUrl,
+		Header:  header,
+		Body:    m,
+	}
+	r := RespJSON{}
+	resp := _client.PostForm(ctx, req)
+	if resp.Err() != nil {
+		fmt.Println("resp Error: ", resp.Err())
+		return
+	}
+	str, err := resp.String()
+	if err != nil {
+		fmt.Println("string error: ", err)
+		return
+	}
+	fmt.Printf("string: %s\n", str)
+	if err = resp.Scan(&r); err != nil {
+		fmt.Println("scan error: ", err)
+		return
+	}
+	fmt.Println("scan: ", r)
+}
+
+func PostJSONInstant() {
+	postUrl := "http://localhost:8282/post/json"
+	m := make(map[string]string)
+	m["param1"] = "123"
+	m["param2"] = "456"
+	m["param3"] = "blabla"
+	m["param4"] = "lkwmef"
+	postJson := &_client.Request{
+		BaseURL: postUrl,
+		Body:    m,
+	}
+	ctx := context.Background()
+	resp := _client.PostJSON(ctx, postJson)
 	if resp.Err() != nil {
 		log.Println("Error: ", resp.Err())
 		return
